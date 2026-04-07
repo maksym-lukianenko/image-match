1d5bcb15e90ab286dfd194bae8ec9a3630109d1a
Maksym Lukianenko <maximkharkow@gmail.com>
2026-04-07 16:08:12 +0200

    feat: convert elasticsearch_driver.py to backward-compat re-export, update __init__ exports

--- image_match/__init__.py
+++ image_match/__init__.py
@@ -1,2 +1,7 @@
 __author__ = 'ryan'
 __version__ = '2.0.0'
+
+from image_match.elasticsearch_driver_es7 import SignatureES7
+from image_match.elasticsearch_driver_es8 import SignatureES8
+
+__all__ = ['SignatureES7', 'SignatureES8']
