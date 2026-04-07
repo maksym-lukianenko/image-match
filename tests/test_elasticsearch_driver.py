b5d498a5b5e6226eb8fb355e522ccb7494a3c01f
Maksym Lukianenko <maximkharkow@gmail.com>
2026-04-07 16:09:55 +0200

    test: update ES driver test to use SignatureES7, add deprecation warning test

--- tests/test_elasticsearch_driver.py
+++ tests/test_elasticsearch_driver.py
@@ -1,15 +1,15 @@
-import pytest
-import os
 import hashlib
-try:
-    from urllib.request import urlretrieve
-except:
-    from urllib import urlretrieve
-from elasticsearch import Elasticsearch, ConnectionError, RequestError, NotFoundError
+import os
+import warnings
 from time import sleep
 
-from image_match.elasticsearch_driver import SignatureES
+import pytest
+from elasticsearch import Elasticsearch, ConnectionError, NotFoundError, RequestError
 from PIL import Image
+from urllib.request import urlretrieve
+
+from image_match.elasticsearch_driver import SignatureES
+from image_match.elasticsearch_driver_es7 import SignatureES7
 
 test_img_url1 = 'https://camo.githubusercontent.com/810bdde0a88bc3f8ce70c5d85d8537c37f707abe/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f652f65632f4d6f6e615f4c6973612c5f62795f4c656f6e6172646f5f64615f56696e63692c5f66726f6d5f4332524d465f7265746f75636865642e6a70672f36383770782d4d6f6e615f4c6973612c5f62795f4c656f6e6172646f5f64615f56696e63692c5f66726f6d5f4332524d465f7265746f75636865642e6a7067'
 test_img_url2 = 'https://camo.githubusercontent.com/826e23bc3eca041110a5af467671b012606aa406/68747470733a2f2f63322e737461746963666c69636b722e636f6d2f382f373135382f363831343434343939315f303864383264653537655f7a2e6a7067'
@@ -23,14 +23,10 @@ MAPPINGS = {
         "properties": {
             DOC_TYPE: {
                 "properties": {
-                    "path": {
-                        "type": "keyword"
-                    },
+                    "path": {"type": "keyword"},
                     "metadata": {
                         "properties": {
-                            "tenant_id": {
-                                "type": "keyword",
-                            }
+                            "tenant_id": {"type": "keyword"}
                         }
                     }
                 }
@@ -44,13 +40,14 @@ MAPPINGS = {
 def index_name():
     return INDEX_NAME
 
+
 @pytest.fixture(scope='function', autouse=True)
 def setup_index(request, index_name):
     es = Elasticsearch()
     try:
         es.indices.create(index=index_name, body=MAPPINGS)
     except RequestError as e:
-        if e.error == u'resource_already_exists_exception':
+        if e.error == 'resource_already_exists_exception':
             es.indices.delete(index_name)
         else:
             raise
@@ -63,6 +60,7 @@ def setup_index(request, index_name):
 
     request.addfinalizer(fin)
 
+
 @pytest.fixture(scope='function', autouse=True)
 def cleanup_index(request, es, index_name):
     def fin():
@@ -70,29 +68,37 @@ def cleanup_index(request, es, index_name):
             es.indices.delete(index_name)
         except NotFoundError:
             pass
+
     request.addfinalizer(fin)
+
 
 @pytest.fixture
 def es():
     return Elasticsearch()
 
+
 @pytest.fixture
 def ses(es, index_name):
-    return SignatureES(es=es, index=index_name, doc_type=DOC_TYPE)
+    return SignatureES7(es=es, index=index_name, doc_type=DOC_TYPE)
+
 
 def test_elasticsearch_running(es):
-    i = 0
-    while i < 5:
+    for i in range(5):
         try:
             es.ping()
-            assert True
             return
         except ConnectionError:
-            i += 1
             sleep(2)
+    pytest.fail('Elasticsearch not running (failed to connect after 5 tries)')
 
-    pytest.fail('Elasticsearch not running (failed to connect after {} tries)'
-                .format(str(i)))
+
+def test_signaturees_emits_deprecation_warning(es, index_name):
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+        SignatureES(es=es, index=index_name, doc_type=DOC_TYPE)
+    assert len(w) == 1
+    assert issubclass(w[0].category, DeprecationWarning)
+    assert 'SignatureES7' in str(w[0].message)
 
 
 def test_add_image_by_url(ses):
@@ -142,6 +148,7 @@ def test_lookup_from_file(ses):
     assert 'dist' in r[0]
     assert 'id' in r[0]
 
+
 def test_lookup_from_bytestream(ses):
     ses.add_image('test1.jpg', refresh_after=True)
     with open('test1.jpg', 'rb') as f:
@@ -152,27 +159,16 @@ def test_lookup_from_bytestream(ses):
     assert 'dist' in r[0]
     assert 'id' in r[0]
 
+
 def test_lookup_with_cutoff(ses):
     ses.add_image('test2.jpg', refresh_after=True)
-    ses.distance_cutoff=0.01
+    ses.distance_cutoff = 0.01
     r = ses.search_image('test1.jpg')
     assert len(r) == 0
 
 
-def check_distance_consistency(ses):
-    ses.add_image('test1.jpg')
-    ses.add_image('test2.jpg', refresh_after=True)
-    r = ses.search_image('test1.jpg')
-    assert r[0]['dist'] == 0.0
-    assert r[-1]['dist'] == 0.42672771706789686
-
-
 def test_add_image_with_metadata(ses):
-    metadata = {'some_info':
-                    {'test':
-                         'ok!'
-                     }
-                }
+    metadata = {'some_info': {'test': 'ok!'}}
     ses.add_image('test1.jpg', metadata=metadata, refresh_after=True)
     r = ses.search_image('test1.jpg')
     assert r[0]['metadata'] == metadata
@@ -183,25 +179,20 @@ def test_add_image_with_metadata(ses):
 
 
 def test_lookup_with_filter_by_metadata(ses):
-    metadata = dict(
-            tenant_id='foo'
-    )
+    metadata = {'tenant_id': 'foo'}
     ses.add_image('test1.jpg', metadata=metadata, refresh_after=True)
-
-    metadata2 = dict(
-            tenant_id='bar-2'
-    )
+    metadata2 = {'tenant_id': 'bar-2'}
     ses.add_image('test2.jpg', metadata=metadata2, refresh_after=True)
 
-    r = ses.search_image('test1.jpg', pre_filter={"term": {'{}.metadata.tenant_id'.format(DOC_TYPE): "foo"}})
+    r = ses.search_image('test1.jpg', pre_filter={"term": {f'{DOC_TYPE}.metadata.tenant_id': "foo"}})
     assert len(r) == 1
     assert r[0]['metadata'] == metadata
 
-    r = ses.search_image('test1.jpg', pre_filter={"term": {'{}.metadata.tenant_id'.format(DOC_TYPE): "bar-2"}})
+    r = ses.search_image('test1.jpg', pre_filter={"term": {f'{DOC_TYPE}.metadata.tenant_id': "bar-2"}})
     assert len(r) == 1
     assert r[0]['metadata'] == metadata2
 
-    r = ses.search_image('test1.jpg', pre_filter={"term": {'{}.metadata.tenant_id'.format(DOC_TYPE): "bar-3"}})
+    r = ses.search_image('test1.jpg', pre_filter={"term": {f'{DOC_TYPE}.metadata.tenant_id': "bar-3"}})
     assert len(r) == 0
 
 
@@ -213,12 +204,12 @@ def test_all_orientations(ses):
     r = ses.search_image('rotated_test1.jpg', all_orientations=True)
     assert len(r) == 1
     assert r[0]['path'] == 'test1.jpg'
-    assert r[0]['dist'] < 0.05  # some error from rotation
+    assert r[0]['dist'] < 0.05
 
     with open('rotated_test1.jpg', 'rb') as f:
         r = ses.search_image(f.read(), bytestream=True, all_orientations=True)
         assert len(r) == 1
-        assert r[0]['dist'] < 0.05  # some error from rotation
+        assert r[0]['dist'] < 0.05
 
 
 def test_duplicate(ses):
